beb1139b25fe6532488ee71d6b121c0281a43bda
Glitch2589 <41716508+Glitch2589@users.noreply.github.com>
2020-05-29 20:04:57 +0700

    Add files via upload

---
+++ WebappDatabase_0.1.sql
@@ -0,0 +1,24 @@
+create database WebAppProj;
+go
+
+create table Streamer(
+fullname varchar(30),
+username varchar(30),
+password varchar(30),
+email varchar(30),
+age int,
+donated int,
+information varchar(500),
+hirePrice int,
+star int
+)
+
+create table (
+fullname varchar(30),
+username varchar(30),
+password varchar(30),
+email varchar(30),
+age int,
+donated int
+)
+
